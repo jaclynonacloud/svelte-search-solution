f78f97e422565fadd92944b75b1af6a3db8ec96d
Jaclyn Staples <jaclynonacloud@gmail.com>
2021-04-23 16:55:37 -0300

    Added toggle group functionality.

--- src/components/atoms/ToggleButton.svelte
+++ src/components/atoms/ToggleButton.svelte
@@ -1,3 +1,28 @@
-<button class="toggle-button">
-    button
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const emit = createEventDispatcher()
+    export let readable='button'
+
+    export let isActive = false
+
+    const handleClick = () => {
+        isActive = !isActive
+        emit('selected')
+    }
+</script>
+
+<button class="toggle-button {isActive ? 'active' : ''}" on:click={handleClick}>
+    {@html readable}
 </button>
+
+<style lang="scss">
+    .toggle-button {
+        &:hover {
+            cursor: pointer;
+        }
+        &.active {
+            background: green;
+        }
+    }
+</style>
--- src/components/molecules/ToggleButtonGroup.svelte
+++ src/components/molecules/ToggleButtonGroup.svelte
@@ -1,3 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import ToggleButton from "../atoms/ToggleButton.svelte"
+
+    const emit = createEventDispatcher()
+
+    export let value = ''
+    export let options = ['hello', 'world']
+
+    const handleSelected = option => {
+        value = option
+        emit('input', value)
+    }
+</script>
+
 <div class="toggle-button-group">
-    toggle button group...
+    {#each options as option}
+    <ToggleButton readable={option} on:selected={() => handleSelected(option)} isActive={value === option} />
+    {/each}
 </div>
