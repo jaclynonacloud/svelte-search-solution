b73aac6ac737529c6c8779854af3276f2dc252b5
Jaclyn Staples <jaclynonacloud@gmail.com>
2021-04-23 16:23:29 -0300

    Adding sass to solution.

--- src/components/SearchSolution.svelte
+++ src/components/SearchSolution.svelte
@@ -1,3 +1,17 @@
 <section class="search-solution">
     Search Solution
+    <div class="search-solution__hello">
+        Hi
+    </div>
 </section>
+
+<style lang="scss">
+    .search-solution {
+        background: #4a4a75;
+        color: white;
+        
+        &__hello {
+            background: #5a2d2d;
+        }
+    }
+</style>
