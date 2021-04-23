c8e46533bb59c6e5f5fb58ff6034475f07ca15a4
Jaclyn Staples <jaclynonacloud@gmail.com>
2021-04-23 17:01:52 -0300

    Adding results list functionality.

--- src/components/atoms/SearchResult.svelte
+++ src/components/atoms/SearchResult.svelte
@@ -1,3 +1,21 @@
+<script>
+    export let readable = 'readable'
+    export let id = 'id'
+</script>
+
 <div class="search-result">
-    search result
+    <div class="search-result__readable">{@html readable}</div>
+    <div class="search-result__id">{@html id}</div>
 </div>
+
+<style lang="scss">
+    .search-result {
+        display: flex;
+        justify-content: space-between;
+
+        &__id {
+            font-size: 0.9em;
+            opacity: 0.6;
+        }
+    }
+</style>
--- src/components/molecules/SearchResultList.svelte
+++ src/components/molecules/SearchResultList.svelte
@@ -1,3 +1,14 @@
+<script>
+    import SearchResult from "../atoms/SearchResult.svelte";
+
+    export let results = [
+        { id: 'carrot', readable: 'Carrot' },
+        { id: 'zucchini', readable: 'Zucchini'}
+    ]
+</script>
+
 <div class="search-result-list">
-    search result list...
+    {#each results as {readable, id}}
+    <SearchResult {readable} {id} />
+    {/each}
 </div>
