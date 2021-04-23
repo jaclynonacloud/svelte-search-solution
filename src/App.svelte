56587a13d5f2d1f5af750493a64222dec85e415f
Jaclyn Staples <jaclynonacloud@gmail.com>
2021-04-23 16:14:05 -0300

    Created basic solution component.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
 <script>
-	export let name;
+	import SearchSolution from "./components/SearchSolution.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<SearchSolution />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	:global(html),
+	:global(body) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: grey;
 	}
 </style>
